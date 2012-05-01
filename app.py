dfdbeefb77b6600f315d5584af55c378f1416fbd
takada <takada2@takada-mba.local>
2012-05-02 01:57:26 +0900

    twitter link

--- app.py
+++ app.py
@@ -2,17 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import os
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,url_for
+import urllib
 app = Flask(__name__)
 import phigen
 
 @app.route("/")
 def gen():
     inputstr = request.values.get('input', u'プラトン')
-    inputstr.encode('utf_8')
     g = phigen.Phigen()
     res = g.gen(inputstr)
-    return render_template('template.html', author=inputstr, title=res['title'], contents=res['story'])
+    url = "http://vivid-mist-6790.herokuapp.com?input=" + urllib.quote(inputstr.encode('utf-8'))
+    summary = u"#哲学論文ジェネレーター 著者: %s タイトル：%s 要約: %s %s" %(inputstr, res['title'], res['story'], url)
+    twitter = 'http://twitter.com/home?status=%s' % urllib.quote(summary.encode('utf-8'))
+    return render_template('template.html', author=inputstr, title=res['title'], contents=res['story'], twitter=twitter)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
